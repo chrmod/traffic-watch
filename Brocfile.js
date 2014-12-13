a0d17e31f3f542ec11a84133f94fa7963293ce34
piotrzientara <piotr.zientara@rebased.pl>
2014-12-13 23:00:52 +0100

    chart mockup

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,10 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+app.import('bower_components/d3/d3.js');
+app.import('bower_components/lodash/lodash.js');
+app.import('bower_components/ember-charts/dist/ember-charts.js');
+app.import('bower_components/ember-charts/dist/ember-charts.css');
+
 
 module.exports = app.toTree();
--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,4 +1,89 @@
 import Ember from 'ember';
+// import d3 from 'd3';
 
 export default Ember.Controller.extend({
+
+  timeSeriesBarContent: [
+    {
+      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
+      label: "Avarage density in Warsaw",
+      value: 49668,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
+      label: "Avarage density in Warsaw",
+      value: 68344,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
+      label: "Avarage density in Warsaw",
+      value: 60654,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
+      label: "Avarage density in Warsaw",
+      value: 48240,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
+      label: "Avarage density in Warsaw",
+      value: 62074,
+      type: "money"
+    }
+  ],
+
+  // Used only for time series chart
+  timeSeriesLineContent: [
+    {
+      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
+      label: "Warsaw",
+      value: 17326,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
+      label: "Cracow",
+      value: 4515,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
+      label: "Warsaw",
+      value: 15326,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
+      label: "Cracow",
+      value: 1515,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
+      label: "Warsaw",
+      value: 14326,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
+      label: "Cracow",
+      value: 8518,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
+      label: "Warsaw",
+      value: 42301,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
+      label: "Cracow",
+      value: 90191,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
+      label: "Warsaw",
+      value: 57326,
+      type: "money"
+    }, {
+      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
+      label: "Cracow",
+      value: 39544,
+      type: "money"
+    }
+  ]
 });
