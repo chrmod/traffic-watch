27c5bac5e2921f96e698db1fbd0c6dca5147e03e
piotrzientara <piotr.zientara@rebased.pl>
2014-12-14 21:23:34 +0100

    chart mockup settings with hour interval

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,89 +1,137 @@
 import Ember from 'ember';
-// import d3 from 'd3';
+/* global d3 */
 
 export default Ember.Controller.extend({
 
-  timeSeriesBarContent: [
-    {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-      label: "Avarage density in Warsaw",
-      value: 49668,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-      label: "Avarage density in Warsaw",
-      value: 68344,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-      label: "Avarage density in Warsaw",
-      value: 60654,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-      label: "Avarage density in Warsaw",
-      value: 48240,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-      label: "Avarage density in Warsaw",
-      value: 62074,
-      type: "money"
-    }
-  ],
+  timeSeriesBarContent: [],
+
+  //   {
+  //     time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T9:00:00Z"),
+  //     label: "Avarage density in Warsaw",
+  //     value: 50
+
+  //   }, {
+  //     time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T12:00:00Z"),
+  //     label: "Avarage density in Warsaw",
+  //     value: 40
+
+  //   }, {
+  //     time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T15:00:00Z"),
+  //     label: "Avarage density in Warsaw",
+  //     value: 60
+
+  //   }, {
+  //     time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T18:00:00Z"),
+  //     label: "Avarage density in Warsaw",
+  //     value: 55
+
+  //   }, {
+  //     time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T21:00:00Z"),
+  //     label: "Avarage density in Warsaw",
+  //     value: 30
+
+  //   }
+  // ],
 
   // Used only for time series chart
   timeSeriesLineContent: [
     {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T00:00:00Z"),
       label: "Warsaw",
-      value: 17326,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-05-15"),
-      label: "Cracow",
-      value: 4515,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
+      value: 20
+    },
+    {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T01:00:00Z"),
       label: "Warsaw",
-      value: 15326,
-      type: "money"
+      value: 34
     }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-06-15"),
-      label: "Cracow",
-      value: 1515,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T02:00:00Z"),
       label: "Warsaw",
-      value: 14326,
-      type: "money"
+      value: 36
     }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-07-16"),
-      label: "Cracow",
-      value: 8518,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T03:00:00Z"),
       label: "Warsaw",
-      value: 42301,
-      type: "money"
+      value: 40
     }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-08-16"),
-      label: "Cracow",
-      value: 90191,
-      type: "money"
-    }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T04:00:00Z"),
       label: "Warsaw",
-      value: 57326,
-      type: "money"
+      value: 45
     }, {
-      time: d3.time.format('%Y-%m-%d').parse("2013-09-16"),
-      label: "Cracow",
-      value: 39544,
-      type: "money"
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T05:00:00Z"),
+      label: "Warsaw",
+      value: 50
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T06:00:00Z"),
+      label: "Warsaw",
+      value: 70
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T07:00:00Z"),
+      label: "Warsaw",
+      value: 64
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T08:00:00Z"),
+      label: "Warsaw",
+      value: 71
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T09:00:00Z"),
+      label: "Warsaw",
+      value: 74
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T10:00:00Z"),
+      label: "Warsaw",
+      value: 80
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T11:00:00Z"),
+      label: "Warsaw",
+      value: 79
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T12:00:00Z"),
+      label: "Warsaw",
+      value: 70
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T13:00:00Z"),
+      label: "Warsaw",
+      value: 67
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T14:00:00Z"),
+      label: "Warsaw",
+      value: 69
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T15:00:00Z"),
+      label: "Warsaw",
+      value: 64
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T16:00:00Z"),
+      label: "Warsaw",
+      value: 66
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T17:00:00Z"),
+      label: "Warsaw",
+      value: 56
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T18:00:00Z"),
+      label: "Warsaw",
+      value: 51
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T19:00:00Z"),
+      label: "Warsaw",
+      value: 45
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T20:00:00Z"),
+      label: "Warsaw",
+      value: 30
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T21:00:00Z"),
+      label: "Warsaw",
+      value: 34
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T22:00:00Z"),
+      label: "Warsaw",
+      value: 36
+    }, {
+      time: d3.time.format('%Y-%m-%dT%H:%M:%SZ').parse("2014-07-01T23:00:00Z"),
+      label: "Warsaw",
+      value: 30
     }
   ]
 });
