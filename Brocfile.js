a3185d9efef41ee80700c9eaf6ff3c7e83b5d061
piotrzientara <piotr.zientara@rebased.pl>
2014-12-28 19:32:44 +0100

    layout improvements, bootstrap addded

--- Brocfile.js
+++ Brocfile.js
@@ -21,5 +21,14 @@ app.import('bower_components/lodash/lodash.js');
 app.import('bower_components/ember-charts/dist/ember-charts.js');
 app.import('bower_components/ember-charts/dist/ember-charts.css');
 
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', { destDir: 'assets' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', { destDir: 'fonts' });
+app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
+
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
 
 module.exports = app.toTree();
