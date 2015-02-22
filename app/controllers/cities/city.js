83864e70959fd028176867fbd89e5fb43ef24fa7
piotrzientara <piotr.zientara@rebased.pl>
2015-02-22 19:49:03 +0100

    small clean up

--- app/controllers/cities/city.js
+++ app/controllers/cities/city.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import {MAP_TYPES} from '../../components/google-map';
+import d3 from '../../utils/d3';
 
 export default Ember.ObjectController.extend({
   needs: ['cities'],
 
-  timeSeriesBarContent: [],
+// MAP
 
   lat: function () {
     return this.get('center').split(',')[0];
@@ -18,6 +19,8 @@ export default Ember.ObjectController.extend({
   type:        'road',
   mapTypes:    MAP_TYPES,
 
+// CHART
+  timeSeriesBarContent: [],
   timeSeriesLineContent: Ember.computed.map('model.stats', function (stat) {
     return {
       time: d3.time.format.iso.parse(stat.get('time')),
